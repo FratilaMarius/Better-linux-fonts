ee3e8b9888ad711509ee2dd9a85dd667326d7cfe
FratilaMarius <marius.fratila123@gmail.com>
2026-09-11 04:03:25 +0300

    Update cssfrm.c

--- cssfrm.c
+++ cssfrm.c
@@ -19,148 +19,134 @@ call with funcPointers = NULL for MENU to return just input (int)
 */
 #include "cssfrm.h"
 
-#define TEXT_COLOUR(colourCode) printf("\033[%dm", colourCode)
-#define TEXT_RESET() printf("\033[0m")
-#define TEXT_BOLD() printf("\033[1m")
-#define TEXT_UNDERLINE() printf("\033[4m")
-#define BACKGROUND_COLOUR(colourCode) printf("\033[%dm", colourCode)
-#define BACKGROUND_RESET() printf("\033[49m")
-
-void __StringToInt_forColors__(char *color) {
-  if(!strncmp(color, "blk", 3)) color[0] = 30;
-  if(!strncmp(color, "red", 3)) color[0] = 31;
-  if(!strncmp(color, "grn", 3)) color[0] = 32;
-  if(!strncmp(color, "yel", 3)) color[0] = 33;
-  if(!strncmp(color, "blu", 3)) color[0] = 34;
-  if(!strncmp(color, "mag", 3)) color[0] = 35;
-  if(!strncmp(color, "cyn", 3)) color[0] = 36;
-  if(!strncmp(color, "wht", 3)) color[0] = 37;
+int GetColorCode(const char *color) {
+    if(!color) return 37;
+    if(!strncmp(color, "blk", 3)) return 30;
+    if(!strncmp(color, "red", 3)) return 31;
+    if(!strncmp(color, "grn", 3)) return 32;
+    if(!strncmp(color, "yel", 3)) return 33;
+    if(!strncmp(color, "blu", 3)) return 34;
+    if(!strncmp(color, "mag", 3)) return 35;
+    if(!strncmp(color, "cyn", 3)) return 36;
+    if(!strncmp(color, "wht", 3)) return 37;
+    return 37; // Default to white
 }
 
 void Format(char *input, ...) {
-  va_list lista_arg;
-  va_start(lista_arg, input);
-  char color[4] = {0}, *background[4] = {0}, bold, underline, itallic;
-  // defaults:
-  bold = 22;
-  itallic = 23;
-  underline= 24;
-  color[0] = 37;
-  background [0] = 49;
-// getting the number of characters in the string:
-  int counter = 0;
-  while (input[counter] != 0) {
-    counter++;
-    if(counter == 500) {
-      printf("Error in reading number of chars in string input");
-      return;
-    }
-  }
-// parcurge stringul si ia inputul:
-  for (int j = 0; j < counter; j++) {
-    if(input[j] == 0x23 && input[j+1] != 0x0) {
-      if(input[j+1] == 99) {
-        strncpy(color, va_arg(lista_arg, char*), 4);
-        __StringToInt_forColors__(color);
-      }
-      if(input[j+1] == 98) {
-        strncpy(background,va_arg(lista_arg, char*), 4);
-        __StringToInt_forColors__(background); background[0] += 10;
-      }
-      if(input[j+1] == 66) {
-        strncpy(&bold, va_arg(lista_arg, char*), 1);
-        if(bold == 121) bold = 1;
-      }
-      if(input[j+1] == 117) {
-        strncpy(&underline, va_arg(lista_arg, char*), 1);
-        if(underline == 121) underline = 4;
-      }
-      if(input[j+1] == 105) {
-        strncpy(&itallic, va_arg(lista_arg, char*), 1);
-        if(underline == 121) itallic = 3;
-      }
-      j++;
-    } 
-  }
-  // color si background[0] sunt seatate cum trebuie si acu mai tre sa faci la celelalte 3 si sq
-  // vezi cum faci printarea
-  printf("\033[%d;%d;%d;%d;%dm", color[0], background[0], bold, itallic, underline);
+    if (!input) return;
 
-  va_end(lista_arg);
+    va_list lista_arg;
+    va_start(lista_arg, input);
+    
+    // ANSI Defaults
+    int color = 37;
+    int background = 49;
+    int bold = 22;
+    int itallic = 23;
+    int underline = 24;
+
+    int counter = strlen(input);
+
+    // Loop stops at counter - 1 to safely check j+1
+    for (int j = 0; j < counter - 1; j++) {
+        if(input[j] == '#') {
+            if(input[j+1] == 'c') {
+                color = GetColorCode(va_arg(lista_arg, char*));
+            }
+            else if(input[j+1] == 'b') {
+                background = GetColorCode(va_arg(lista_arg, char*)) + 10;
+            }
+            else if(input[j+1] == 'B') {
+                char *val = va_arg(lista_arg, char*);
+                if(val && val[0] == 'y') bold = 1;
+            }
+            else if(input[j+1] == 'u') {
+                char *val = va_arg(lista_arg, char*);
+                if(val && val[0] == 'y') underline = 4;
+            }
+            else if(input[j+1] == 'i') {
+                char *val = va_arg(lista_arg, char*);
+                if(val && val[0] == 'y') itallic = 3;
+            }
+            j++; // Skip the format character so it isn't parsed again
+        } 
+    }
+    
+    printf("\033[%d;%d;%d;%d;%dm", color, background, bold, itallic, underline);
+    va_end(lista_arg);
 }
 
 void Message(const char *message) {
-  Format("#c #i", "grn", "y");
-  printf("%s", message);
-  Format("");
+    Format("#c #i", "grn", "y");
+    printf("%s", message);
+    Format("");
 }
+
 void Error(const char *errorMessage) {
-  Format("#c#B", "red", "y");
-  perror(errorMessage);
-  Format("");
+    Format("#c#B", "red", "y");
+    perror(errorMessage);
+    Format("");
 }
 
 void GUI_1(char *declaredNames[], int nrOfEntries) {
-  printf("\n\n_MENU_:");
-  
-  for(int i = 0; i < nrOfEntries; i++) {
-    printf("\n     [%d]...%s", i+1 ,declaredNames[i]);
-  }
-  printf("\n[OPTION BY NUMBER] --> ");
+    printf("\n\n_MENU_:");
+    for(int i = 0; i < nrOfEntries; i++) {
+        printf("\n     [%d]...%s", i+1, declaredNames[i]);
+    }
+    printf("\n[OPTION BY NUMBER] --> ");
 }
+
 void GUI_2(char *decNames[], int nr) {
-  Format("#c#B", "blu", "y");
-
-  printf("\nCommands:\n=============================================================================");
-  Format("#c#B", "cyn", "y");
-  for(int i = 0; i < nr; i++) {
-    printf("\n     [%d]...%s", i+1 ,decNames[i]);
-  }
-  Format("#c#B", "blu", "y");
-  printf("\n=============================================================================\n[Enter command id] --> ");
-  Format("");
+    Format("#c#B", "blu", "y");
+    printf("\nCommands:\n=============================================================================");
+    Format("#c#B", "cyn", "y");
+    for(int i = 0; i < nr; i++) {
+        printf("\n     [%d]...%s", i+1, decNames[i]);
+    }
+    Format("#c#B", "blu", "y");
+    printf("\n=============================================================================\n[Enter command id] --> ");
+    Format("");
 }
-int Menu(char *declaredNames[], void *funcPointers[], int GUI_style) {
-  //asses how many UI and function ellements there are:
-  int nrOfEntries_fncP = 0;
-  int nrOfEntries = 0;
-  if (funcPointers == NULL) 
-    for(int i = 0; declaredNames[i] != NULL; i++) {
-      nrOfEntries++;
-    }
-  if (funcPointers != NULL) {
-    for(int i = 0; declaredNames[i] != NULL; i++) {
-      nrOfEntries++;
-    }
-    for(int i = 0; funcPointers[i] != NULL; i++) {
-      nrOfEntries_fncP++;
-    }
-    if(nrOfEntries != nrOfEntries_fncP) {
-      printf("\n\nIncorect use of menu function");
-      exit(1);
-    }
-  }
-  // GUI:
-  switch (GUI_style) {
-    case 1:
-      GUI_1(declaredNames, nrOfEntries);
-      break;
-    case 2:
-      GUI_2(declaredNames, nrOfEntries);
-      break;
 
-    default: 
-    printf("\nUnrecognised UI element!"); 
-    exit(1);
-      break;
-  }
-  // take the input:
-  int input = -1;
-  scanf("%d", &input);
-  if (funcPointers != NULL && input != -1) {
-    ((void (*)())funcPointers[input - 1])();  
-    return 0;
-  }
-  if(input != -1) return input;
-  else printf("Error in recieving menu input!\n");
+int Menu(char *declaredNames[], void *funcPointers[], int GUI_style) {
+    int nrOfEntries_fncP = 0;
+    int nrOfEntries = 0;
+    
+    if (funcPointers == NULL) {
+        for(int i = 0; declaredNames[i] != NULL; i++) nrOfEntries++;
+    } else {
+        for(int i = 0; declaredNames[i] != NULL; i++) nrOfEntries++;
+        for(int i = 0; funcPointers[i] != NULL; i++) nrOfEntries_fncP++;
+        
+        if(nrOfEntries != nrOfEntries_fncP) {
+            printf("\n\nIncorrect use of menu function: Name and Function arrays must match in size.\n");
+            exit(1);
+        }
+    }
+
+    switch (GUI_style) {
+        case 1: GUI_1(declaredNames, nrOfEntries); break;
+        case 2: GUI_2(declaredNames, nrOfEntries); break;
+        default: 
+            printf("\nUnrecognised UI element!\n"); 
+            exit(1);
+    }
+
+    // Secure input gathering
+    char inputBuffer[16];
+    int input = -1;
+    
+    if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != NULL) {
+        input = strtol(inputBuffer, NULL, 10);
+    }
+
+    if (funcPointers != NULL && input > 0 && input <= nrOfEntries) {
+        ((void (*)())funcPointers[input - 1])();  
+        return 0;
+    }
+    
+    if(input > 0 && input <= nrOfEntries) return input;
+    
+    printf("Error: Invalid menu input!\n");
+    return -1;
 }
